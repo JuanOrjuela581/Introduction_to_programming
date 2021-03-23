302ac2c72f785cc528f8c97d2a294a64759ea909
Juan Pablo Orjuela <juan.orjuelar@usantoto.edu.co>
2021-03-23 09:17:35 -0500

    Upload JAVA

---
+++ Exercises JAVA/P1T37_JAVA_METHODS1/src/usta/fis/Main.java
@@ -0,0 +1,23 @@
+package usta.fis;
+
+//Author       :Juan Orjuela
+//Date         :23 March of 2021
+//Description  :this software use methods in your code
+
+public class Main {
+
+    public static void main(String[] args) {
+	p_show_Info_program();
+    }
+
+    public static void p_show_Info_program() {
+        //DATE       : 23 March of 2021
+        //DESCRIPTION: This method show the info software.
+        System.out.println("------------------------");
+        System.out.println("-SoftMethod Version 1.0-");
+        System.out.println("MAKER: Juan Orjuela");
+        System.out.println("DESCR: used methods");
+        System.out.println("------------------------");
+    }
+
+}

a4e005e95d5295da6ff625fe6224bf310c9cd68e
Juan Pablo Orjuela <juan.orjuelar@usantoto.edu.co>
2021-06-18 16:35:00 -0500

    Upload Python

---
+++ python/basic elements/adult_or_child/age.py
@@ -0,0 +1,11 @@
+"""
+Author: Juan Orjuela
+Date: June 18 of 2021
+"""
+name = input("Please type your name: ")
+age = int(input("Please type your age"))
+
+if age<18:
+    print("hello",name,"You are younger")
+else:
+    print("hello",name,"You are adult")
---
+++ python/basic elements/hello/hello.py
@@ -0,0 +1,8 @@
+"""
+Author: Juan Orjuela
+Date: June 18 of 2021
+"""
+
+name = input("Please type your name: ")
+
+print(f"Hello {name}")
